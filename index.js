695a01d2120dc0e6b686426c065190a03d827da3
ramonnokia00 <ramonsoares491@gmail.com>
2025-05-10 01:57:42 -0300

    finalizado

--- index.js
+++ index.js
@@ -1,19 +1,28 @@
+
+
 function buscarCep() {
     try {
-     let cepValue = document.querySelector("#cep").value;
-    fetch(`https://viacep.com.br/ws/${cepValue}/json/`)
-    .then(res => res.json())
-    .then(res => {
-        console.log(res);
-    })   
-    .catch(error => {
-        alert("errou ai ne migão vamo colocar um cep certo");
-    })
-    .finally(() => {
-        alert("OK");
-    })
-    } catch (error) {
-        alert("escreveu algo errado ai no codigo migão");
+        let cepValue = document.querySelector("#cep").value;
+        
+
+        fetch(`https://viacep.com.br/ws/${cepValue}/json/`)
+            .then(res => res.json())
+            .then(data => {
+                console.log(data);
+               document.querySelector("#endereco").value = data.logradouro || ""; 
+               document.querySelector("#bairro").value = data.bairro || ""; 
+               document.querySelector("#cidade").value = data.uf || ""; 
+               document.querySelector("#estado").value = data.estado || ""; 
+            })
+            .catch(error => {
+                alert(error, "errou ai ne migão vamo colocar um cep certo");
+            })
+          
+
     }
-    
+        catch (error) {
+        console.error("Erro inesperado:", error);
+        alert("Erro inesperado ao buscar o CEP.");
+    }
 }
+        
